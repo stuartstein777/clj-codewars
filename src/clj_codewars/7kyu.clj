2bb9ecbd70867c49a802e43798cb10689b6d9a90
stuart <qmstuart@gmail.com>
2020-07-23 21:25:44 +0100

    Added 7KYU (Indexed Capilization) solution.

--- src/clj_codewars/7kyu.clj
+++ src/clj_codewars/7kyu.clj
@@ -1528,3 +1528,19 @@
     (cond (empty? intersections) 0
           :else (apply max (map count intersections)))))
 ;; -------------------------------------------END---
+
+;; Indexed capitalization
+;  Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+;
+; For example:
+;    capitalize("abcdef",[1,2,5]) = "aBCdeF"
+;    capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+; The input will be a lowercase string with no spaces and an array of digits.
+
+(defn capitalize [s idx]
+  (let [idxs (set idx)]
+    (apply str (map-indexed (fn [ix i]
+                              (if (idxs ix)
+                                (str/upper-case (str i))
+                                i)) s))))
+;; -------------------------------------------END---
--- test/clj_codewars/7kyu_test.clj
+++ test/clj_codewars/7kyu_test.clj
@@ -606,3 +606,12 @@
   (is (= (consecutive-letters "abd") false))
   (is (= (consecutive-letters "v") true))
   (is (= (consecutive-letters "abbc") false)))
+
+(deftest indexed-capitalization-tests
+  (is (= (capitalize "abcdef" [1 2 5]) "aBCdeF"))
+  (is (= (capitalize "abcdef" [1 2 5 100]) "aBCdeF"))
+  (is (= (capitalize "codewars" [1 3 5 50]) "cOdEwArs"))
+  (is (= (capitalize "abracadabra" [2 6 9 10]) "abRacaDabRA"))
+  (is (= (capitalize "indexinglessons" [0]) "Indexinglessons"))
+
+  )
