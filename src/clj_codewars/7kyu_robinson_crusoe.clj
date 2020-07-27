821442d69482c0791e7dbc326d0adca996291831
stuart <qmstuart@gmail.com>
2020-07-28 00:41:40 +0100

    Refactored 7KYU Robinson Crusoe

--- src/clj_codewars/7kyu_robinson_crusoe.clj
+++ src/clj_codewars/7kyu_robinson_crusoe.clj
@@ -1,16 +1,11 @@
 (ns clj-codewars.7kyu-robinson-crusoe)
 
-(defn solve
-  [n d ang dist-multiplier angle-multiplier x y]
-  (if (zero? n)
-    [x y]
-    (recur (dec n)
-           (* d dist-multiplier)
-           (* ang angle-multiplier)
-           dist-multiplier
-           angle-multiplier
-           (+ x (* d (Math/cos (Math/toRadians ang))))
-           (+ y (* d (Math/sin (Math/toRadians ang)))))))
-
-(defn crusoe [n d ang dist-mult ang-mult]
-  (solve n d ang dist-mult ang-mult 0 0))
+(defn crusoe [n d ang distance-multiplier angle-multiplier]
+  ((fn [n d ang x y]
+     (if (zero? n)
+       [x y]
+       (recur (dec n)
+              (* d distance-multiplier)
+              (* ang angle-multiplier)
+              (+ x (* d (Math/cos (Math/toRadians ang))))
+              (+ y (* d (Math/sin (Math/toRadians ang))))))) n d ang 0 0))
