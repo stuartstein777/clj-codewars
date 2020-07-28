91c1d2b69d7cffb0879c13dc5d8aad1d1b7e782e
stuart <qmstuart@gmail.com>
2020-07-28 20:54:13 +0100

    Refactored 6KYU - Errors Histogram

--- src/clj_codewars/6kyu_errors_histogram.clj
+++ src/clj_codewars/6kyu_errors_histogram.clj
@@ -2,26 +2,25 @@
   (:require [clojure.string :as str]))
 
 ;; Errors : histogram
-; In a factory a printer prints labels for boxes. The printer uses colors which, for the sake of simplicity, are named
-; with letters from a to z (except letters u, w, x or z that are for errors).
+; In a factory a printer prints labels for boxes. The printer uses colors which, for the sake of simplicity, are
+; named with letters from a to z (except letters u, w, x or z that are for errors).
 ;
-;The colors used by the printer are recorded in a control string. For example a control string would be aaabbbbhaijjjm
-; meaning that the printer used three times color a, four times color b, one time color h then one time color a... and
-; so on.
+; The colors used by the printer are recorded in a control string. For example a control string would be
+; aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one
+; time color a... and so on.
 ;
-;Sometimes there are problems: lack of colors, technical malfunction and a control string is produced e.g.
+; Sometimes there are problems: lack of colors, technical malfunction and a control string is produced e.g.
 ; uuaaaxbbbbyyhwawiwjjjwwxym where errors are reported with letters u, w, x or z.
 ;
-;You have to write a function hist which given a string will output the errors as a string representing a histogram of
-; the encountered errors.
+; You have to write a function hist which given a string will output the errors as a string representing a histogram
+; of the encountered errors.
 ;
-;Format of the output string:
+; Format of the output string:;
+; letter (error letters are sorted in alphabetical order) in a field of 2 characters, a white space, number of error
+; for that letter in a field of 6, as many "*" as the number of errors for that letter and "\r".
 ;
-;letter (error letters are sorted in alphabetical order) in a field of 2 characters, a white space, number of error for
-; that letter in a field of 6, as many "*" as the number of errors for that letter and "\r".
-;
-;The string has a length greater or equal to one and contains only letters from a to z.
-;Examples
+; The string has a length greater or equal to one and contains only letters from a to z.
+; Examples
 ;     s="uuaaaxbbbbyyhwawiwjjjwwxym"
 ;     hist(s) => "u  2     **\rw  5     *****\rx  2     **"
 ;
@@ -41,7 +40,8 @@
   (str k "  " (format "%-6d" v) (apply str (repeat v "*"))))
 
 (defn hist [s]
-  (str/join "\r" (->> (filter #(#{\u \w \x \z} %) s)
-                      (frequencies)
-                      (reduce-kv (fn [acc k v] (conj acc (build-error k v))) [])
-                      (sort))))
+  (->> (filter #(#{\u \w \x \z} %) s)
+       (frequencies)
+       (reduce-kv (fn [acc k v] (conj acc (build-error k v))) [])
+       (sort)
+       (str/join "\r")))
