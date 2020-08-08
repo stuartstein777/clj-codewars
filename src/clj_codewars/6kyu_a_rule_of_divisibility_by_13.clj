9ed7a51d136304e5a6552f59c6fb87b92c8bc6ab
stuart <qmstuart@gmail.com>
2020-08-08 01:36:20 +0100

    Format fix.

--- src/clj_codewars/6kyu_a_rule_of_divisibility_by_13.clj
+++ src/clj_codewars/6kyu_a_rule_of_divisibility_by_13.clj
@@ -33,10 +33,10 @@
 
 (defn thirt [n]
   (let [pow-divs [1 10 9 12 3 4]
-      res (->> (number->digits n)
-               (reverse)
-               (map (fn [a b] (* a b)) (cycle pow-divs))
-               (reduce +))]
+        res (->> (number->digits n)
+                 (reverse)
+                 (map (fn [a b] (* a b)) (cycle pow-divs))
+                 (reduce +))]
     (if (= n res)
       res
       (recur res))))
